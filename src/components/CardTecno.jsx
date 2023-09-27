14fda4e48e1944b60f09743ec1ead071f72211b7
Tiziano Mina <tizianominajo@gmail.com>
2023-09-27 16:46:47 -0300

    Se realizo la tarjeta para mostrar las tecnologías

--- src/components/CardTecno.jsx
+++ src/components/CardTecno.jsx
@@ -1,6 +1,15 @@
-
-export const CardTecno = () => {
+export const CardTecno = ({ technology }) => {
   return (
-    <div>CardTecno</div>
-  )
-}
+    <article className=" w-[250px] mx-auto  text-center  my-10 bg-[#f1f1f1] rounded-xl p-4 space-y-5 flex flex-col justify-around">
+      <img
+        src={technology.img}
+        alt={technology.name}
+        className="mx-auto w-[150px]"
+      />
+      <div className="space-y-3">
+        <p className="text-2xl font-bold text-[#2e2e2e]">{technology.name}</p>
+        <p className="font-semibold ">{technology.level}</p>
+      </div>
+    </article>
+  );
+};
