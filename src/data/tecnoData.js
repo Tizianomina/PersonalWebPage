98bf286560b60db1aae47fdf4bb0ea9b5d9c9680
Tiziano Mina <tizianominajo@gmail.com>
2023-09-27 16:46:32 -0300

    Se agregarón las tecnologías

---
+++ src/data/tecnoData.js
@@ -0,0 +1,61 @@
+import react from "../asset/react.png"
+import python from "../asset/python.png"
+import git from "../asset/git.png"
+import nextjs from "../asset/next-js.png"
+import sql from "../asset/sqlite.png"
+import tailwind from "../asset/tailwindimg.png"
+import jest from "../asset/jest.png"
+import rtl from "../asset/rtl.png"
+
+
+
+export const technologies = [
+    {
+        id : 0,
+        name : "React",
+        level : "Intermedio",
+        img : react
+    },
+    {
+        id : 1,
+        name : "Python",
+        level : "Básico",
+        img : python
+    },
+    {
+        id : 2,
+        name : "SQL",
+        level : "Básico",
+        img : sql
+    },
+    {
+        id : 3,
+        name : "Jest",
+        level : "Básico",
+        img : jest
+    },
+    {
+        id : 4,
+        name : "Tailwind",
+        level : "Intermedio",
+        img : tailwind
+    },
+    {
+        id : 5,
+        name : "React Testing Library",
+        level : "Básico",
+        img : rtl
+    },
+    {
+        id : 6,
+        name : "NextJS",
+        level : "Intermedio",
+        img : nextjs
+    },
+    {
+        id : 7,
+        name : "Git",
+        level : "Intermedio",
+        img : git
+    },
+]
