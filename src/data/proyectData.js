c6f0d5927566e28c94b118c573e4e67de9fe2818
Tiziano Mina <tizianominajo@gmail.com>
2023-09-28 02:39:10 -0300

    Se actulizó la información

---
+++ src/data/proyectData.js
@@ -0,0 +1,42 @@
+import miPagina from "../asset/personalPage.png";
+import heroe from "../asset/Heroes.png";
+import gifs from "../asset/gifs.png";
+import memory from "../asset/Memory.png";
+import cine from "../asset/peliculas.png";
+
+export const proyects = [
+  {
+    id: 0,
+    name: "CinesApp",
+    page: "https://mycine.netlify.app/",
+    code: "https://github.com/Tizianomina/Peliculas-Con-React",
+    img: cine,
+  },
+  {
+    id: 1,
+    name: "TizianoMJ",
+    page: "https://tiziano-mina.netlify.app",
+    code: "https://github.com/Tizianomina/TizianoMJ",
+    img: miPagina,
+  },
+  {
+    id: 2,
+    name: "HeroesApp",
+    page: "https://heroes-marvel-dc-tizianomj.netlify.app/login",
+    code: "https://github.com/Tizianomina/Heroes-spa",
+    img: heroe,
+  },
+  {
+    id: 3,
+    name: "GifApp",
+    page: "https://appgif2023.netlify.app/",
+    img: gifs,
+  },
+  {
+    id: 4,
+    name: "Memory",
+    page: "https://tizianomina.github.io/Memory/",
+    code: "https://github.com/Tizianomina/Memory",
+    img: memory,
+  },
+];
