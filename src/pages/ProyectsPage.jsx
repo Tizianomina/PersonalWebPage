87993a747e228f244ae6cd726fbba93e1c6cad72
Tiziano Mina <tizianominajo@gmail.com>
2023-09-28 02:40:14 -0300

    La página en la que contiene las card se ha completado

---
+++ src/pages/ProyectsPage.jsx
@@ -0,0 +1,18 @@
+import { CardProyect } from "../components/CardProyect"
+import { proyects } from "../data/proyectData"
+
+
+export const ProyectsPage = () => {
+  return (
+    <section className="p-5 bg-[#f1f1f1] pt-20">
+        <p className="text-4xl md:text-6xl text-center font-extrabold text-[#2e2e2e]">Proyectos</p>
+        <div className="md:flex md:flex-wrap md:justify-around">
+            {
+                proyects.map(proyect => (
+                    <CardProyect proyect = {proyect} key = {proyect.id}/>
+                ))
+            }
+        </div>
+    </section>
+  )
+}
